008d2d624953f072e26b98e68f168628059d8527
george escobar <georgeescobar@MacBook-Pro.local>
2025-08-21 01:37:39 -0500

    Fix-Railway-host

--- vite.config.js
+++ vite.config.js
@@ -26,5 +26,11 @@ export default defineConfig({
       'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
       'Access-Control-Allow-Headers': 'Content-Type, Authorization'
     }
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
+    allowedHosts: 'all'
   }
 })
+
